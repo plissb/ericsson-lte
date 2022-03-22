ca422ec918e72311aec764a57d007d0812c161c1
Boris Pliss <pliss.boris@gmail.com>
2022-03-22 14:57:56 +0200

    Automation Image bugfix

--- .space.kts
+++ .space.kts
@@ -1,19 +1,3 @@
 job("build and publish"){
-    container(displayName = "Run & Publish", image = "gradle:4.10.3-jre8-alpine"){
-        shellScript {
-            interpreter = "/bin/bash"
-            content = "./gradlew build publish"
-        }
-    }
-
-//    container(displayName = "Run & Publish", image = "gradle:4.10.3-jre8-alpine") {
-//        kotlinScript { api ->
-//            api.gradlew("build")
-//            try {
-//                api.gradlew("publish")
-//            }catch (e: Exception){
-//                println("Publishing failed: $e")
-//            }
-//        }
-//    }
+    gradlew("openjdk8", "build", "publish")
 }
